67991ca6ecd56c78b7fe3c7144661016f863f89b
mjh613 <MarkJHarris@Mark-J-Harriss-MacBook-Pro.local>
2014-07-13 10:02:01 -0400

    Uploading my plots and R codes
    Did not include data file txt

---
+++ plot1.R
@@ -0,0 +1,5 @@
+dat <- read.table(pipe('grep "^[1-2]/2/2007" "household_power_consumption.txt"'),header=F, sep=';')
+colnames(dat) <-names(read.table('household_power_consumption.txt', header=TRUE,sep=";",nrows=1))
+hist(dat$Global_active_power,xlim=c(0,6),col="red",xlab="Global Active Power (kilowatts)",main="Global Active Power")
+dev.copy(png,width=480,height=480,file="plot1.png")
+dev.off()
---
+++ plot2.R
@@ -0,0 +1,7 @@
+dat <- read.table(pipe('grep "^[1-2]/2/2007" "household_power_consumption.txt"'),header=F, sep=';')
+colnames(dat) <-names(read.table('household_power_consumption.txt', header=TRUE,sep=";",nrows=1))
+datetime <- paste(dat$Date,dat$Time)
+dat$DateTimefixed <- strptime(x=datetime,format="%d/%m/%Y %H:%M:%S")
+plot(dat$DateTimefixed,dat$Global_active_power,type="l",xlab="",ylab="Global Active Power (kilowatts)")
+dev.copy(png,width=480,height=480,file="plot2.png")
+dev.off()
